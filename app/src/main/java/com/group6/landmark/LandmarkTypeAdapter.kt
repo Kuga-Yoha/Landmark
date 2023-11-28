7f787c6d2cb57eeba7c8faaff0faacbbc0b7206b
Se1z3d <aditya.amatya131@gmail.com>
2023-11-28 03:15:08 -0500

    landmark 2nd commit

--- app/src/main/java/com/group6/landmark/LandmarkTypeAdapter.kt
+++ app/src/main/java/com/group6/landmark/LandmarkTypeAdapter.kt
@@ -1,33 +1,47 @@
+// LandmarkTypeAdapter.kt
 package com.group6.landmark
 
+import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class LandmarkTypeAdapter(private val types: List<String>, private val listener: OnItemClickListener) :
+class LandmarkTypeAdapter(private val context: Context) :
     RecyclerView.Adapter<LandmarkTypeAdapter.ViewHolder>() {
 
-    interface OnItemClickListener {
-        fun onItemClick(type: String)
-    }
-
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val textViewType: TextView = itemView.findViewById(R.id.textViewType)
-    }
+    private val landmarkTypes = arrayOf("Old Buildings", "Museums", "Stadiums", "Attractions")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // inflates the card_view_design view
+        // that is used to hold list item
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_landmark_type, parent, false)
+            .inflate(R.layout.landmark_type_item, parent, false)
+
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val type = types[position]
-        holder.textViewType.text = type
-        holder.itemView.setOnClickListener { listener.onItemClick(type) }
+        holder.textView.text = landmarkTypes[position]
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, LandmarkList::class.java)
+            intent.putExtra("type_of_landmark", landmarkTypes[position])
+            context.startActivity(intent)
+        }
     }
 
-    override fun getItemCount(): Int = types.size
+
+    // return the number of the items in the list
+    override fun getItemCount(): Int {
+        return landmarkTypes.size
+    }
+
+    // Holds the views for adding it to image and text
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val textView: TextView = itemView.findViewById(R.id.landmark_type_item_tv)
+    }
 }
